ed25afa7beb6f0bdf84eb1830e943244124287d2
colinfwalsh <colinfwalsh@gmail.com>
2016-03-18 15:45:04 -0400

    Whiley Coyote

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'WhileyCoyote' do
+
+end
+
+target 'UnitTests' do
+	pod 'Expecta', '~> 1.0.0'
+	pod ‘Specta’, ‘~> 1.0.0’
+end
+
--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -11,6 +11,20 @@
     
      */
     
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(25) + 24;
+    do {
+        
+        if ((steps % 10 == true) && (steps > 0)) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        NSLog(@"MEEP! MEEP!");
+        steps++;
+    } while (steps <= anvil);
+    
     // do not alter
     return YES;  //
     ///////////////
